a9475ad78bbde6ab4ce1b7214c3dfdceee2614bd
Jesse Lord <jlord@vectra.ai>
2021-06-25 08:51:53 -0700

    Initial commit

---
+++ src/main/scala/chapter2.scala
@@ -0,0 +1,58 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+object chapter2 {
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(i: Int, acc0: Int, acc1: Int): Int =
+      if (i == 0) acc0
+      else go(i - 1, acc1, acc0 + acc1)
+    go(n, 0, 1)
+  }
+  private def testFib(): Unit = {
+    println(fib(1))
+    println(fib(2))
+    println(fib(3))
+    println(fib(4))
+    println(fib(5))
+    println(fib(6))
+    println(fib(7))
+  }
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean = {
+      if (n >= as.length-1) true
+      else if (!ordered(as(n), as(n+1))) false
+      else go(n+1)
+    }
+    go(0)
+  }
+  private def testSorted(): Unit = {
+    def orderedImplicitOrdering[A](a: A, b: A)(implicit ordering:Ordering[A]): Boolean =
+      ordering.lteq(a, b)
+    def orderedContextBound[A : Ordering](a: A, b: A): Boolean = {
+      val ordering = implicitly[Ordering[A]]
+      ordering.lteq(a, b)
+    }
+    def orderedViewBound[A <% Ordered[A]](a: A, b: A): Boolean =
+      a <= b
+    def orderedImplicitOrdered[A](a: A, b: A)(implicit ev$1: A => Ordered[A]): Boolean =
+      a <= b
+
+    val aInt = Array(1, 2, 3)
+    val sortedTest = isSorted(aInt, orderedImplicitOrdering[Int])
+    assert(sortedTest)
+    val aStr = Array("a", "c", "b")
+    val unSortedTest = isSorted(aStr, orderedContextBound[String])
+    assert(!unSortedTest)
+    val aIntEqual = Array(1, 1)
+    val equalTest = isSorted(aIntEqual, orderedViewBound[Int])
+    assert(equalTest)
+    val aDbl = Array(1.0d, 0.99d)
+    val doubleUnsortedTest = isSorted(aDbl, orderedImplicitOrdered[Double])
+    assert(!doubleUnsortedTest)
+  }
+  def main(args: Array[String]): Unit = {
+    //testFib()
+    testSorted()
+  }
+}
